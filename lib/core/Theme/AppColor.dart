cb90cca58c967cda3ca90eec30bf0f1d27c3f60d
aristoo-11 <mohamed.gherabli@etu.univ-batna2.dz>
2024-07-30 00:51:01 +0100

    Theme for Screen,Add aseets

---
+++ lib/core/Theme/AppColor.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  AppColor._();
+  static const Color primaryColor = Color(0xff33665A);
+  static const Color TextColor = Color(0xff616161);
+}
